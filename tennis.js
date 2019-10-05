7225e2ea7460878893482000ad5a371b492ba006
JayRodRock <Jaredlincenberg@gmail.com>
2019-10-04 19:46:18 -0600

    Added Court Image and centered it and the canvas.

--- tennis.js
+++ tennis.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
 	// UNCOMMENT WHEN TURNING IN
 	//update_scores();
-
-
-
 	var testScore = 10;
 	var gameArea = {
 		canvas : document.getElementById('gameCanvas'),
 		start : function() {
-			this.canvas.height = 500;
-			this.canvas.width = 500;
+			this.canvas.height = 650;
+			this.canvas.width = 400;
 			this.context = this.canvas.getContext('2d')
 			this.frameNumber = 0;
 		},
@@ -17,7 +14,7 @@ $(document).ready(function() {
 			this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 		}
 	}
-	initGame();
+	initGame(gameArea);
 
 	gameArea.start();
 
@@ -26,12 +23,21 @@ $(document).ready(function() {
 
 });
 
-function initGame() {
-	displayCourt();
-	drawLoop();
+function initGame(gameArea) {
+	displayCourt(gameArea);
+	drawLoop(gameArea);
 }
-function displayCourt() {
-	
+function displayCourt(gameArea) {
+	var gameCourt = new Image();
+	//370
+	//629
+	gameCourt.onload = function () {
+		var xCenteringPoint =(gameArea.canvas.width-gameCourt.width)/2;
+		var yCenteringPoint = (gameArea.canvas.height-gameCourt.height)/2;
+		gameArea.context.drawImage(gameCourt,xCenteringPoint,yCenteringPoint);
+		console.log(gameArea.canvas.width);
+	};
+	gameCourt.src = "images/court.png"
 }
 
 
