ad403a643fc3f6d7a6a85959c473e6bf8739ad7b
JayRodRock <Jaredlincenberg@gmail.com>
2019-10-07 12:15:21 -0600

    Made the court image a background image so everything will be above it. Used png with transparentence for the ball.

--- tennis.js
+++ tennis.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 	// UNCOMMENT WHEN TURNING IN
 	//update_scores();
@@ -5,8 +6,8 @@ $(document).ready(function() {
 	var gameArea = {
 		canvas : document.getElementById('gameCanvas'),
 		start : function() {
-			this.canvas.height = 400;
-			this.canvas.width = 650;
+			this.canvas.height = 370;
+			this.canvas.width = 629;
 			this.context = this.canvas.getContext('2d')
 			this.frameNumber = 0;
 		},
@@ -14,10 +15,41 @@ $(document).ready(function() {
 			this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 		}
 	}
+	var tennisBall = {
+		height: 25,
+		width: 25,
+		xPos: 400,
+		yPos: 200,
+		ball: new Image(),
+		set source(file){
+			this.ball.src = file;
+		},
+		draw : function () {
+			tennisBall.ball.onload = function () {
+				var can = tennisBall;
+				gameArea.context.drawImage(this,can.xPos,can.yPos, can.width, can.height);
+				console.log(this);
+			}
+		}
+	}
+	var playerRacket = {
+		height: 50,
+		width: 10,
+		xPos: 100,
+		yPos: 300,
+		draw : function () {
+			// gameArea.context.beginPath();
+			gameArea.context.fillStyle = "#FF0000";
+			gameArea.context.fillRect(this.xPos,this.yPos,this.xPos+this.width,this.yPos+this.height);
+			//gameArea.context.stroke();
+		}
+	}
 	initGame(gameArea);
-
+	
 	gameArea.start();
-
+	tennisBall.source = "images/tennisBallTest.png";
+	tennisBall.draw();
+	playerRacket.draw();
 	// UNCOMMENT WHEN TURNING IN
 	//highscore(testScore);
 
@@ -27,21 +59,10 @@ $(document).ready(function() {
 
 
 function initGame(gameArea) {
-	displayCourt(gameArea);
+	//displayCourt(gameArea);
 	drawLoop(gameArea);
 }
-function displayCourt(gameArea) {
-	var gameCourt = new Image();
-	//370
-	//629
-	gameCourt.onload = function () {
-		var xCenteringPoint =(gameArea.canvas.width-gameCourt.width)/2;
-		var yCenteringPoint = (gameArea.canvas.height-gameCourt.height)/2;
-		gameArea.context.drawImage(gameCourt,xCenteringPoint,yCenteringPoint);
-		console.log(gameArea.canvas.width);
-	};
-	gameCourt.src = "images/court.png"
-}
+
 
 
 function drawLoop(){
